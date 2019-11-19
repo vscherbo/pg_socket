09bae79b1b966457f77c9776fee3234f253e293c
Vladimir Scherbo <vscherbo@gmail.com>
2019-11-19 10:01:11 +0300

    skip sond_send if err_cnt >=3

--- cr-trfn_new_topic_msg.sql
+++ cr-trfn_new_topic_msg.sql
@@ -10,11 +10,17 @@ res varchar;
 BEGIN
 SELECT port INTO loc_port FROM arc_energo.topic WHERE tag = NEW.tag;
 
-    FOR rec IN SELECT tag, ip FROM arc_energo.topic_subs WHERE tag = NEW.tag
+    FOR rec IN SELECT tag, ip, err_cnt FROM arc_energo.topic_subs WHERE tag = NEW.tag AND (err_cnt < 3 OR err_cnt IS NULL)
     LOOP
         host_to := host(rec.ip);
         -- res := NULLIF(sock_send(host_to, loc_port, NEW.msg_id::varchar), '');
         res := sock_send(host_to, loc_port, NEW.msg_id::varchar);
+        IF res = '' THEN
+            UPDATE arc_energo.topic_subs SET err_cnt = 0 WHERE tag = rec.tag AND ip = rec.ip;
+        ELSIF res LIKE '%timed out%' THEN
+            UPDATE arc_energo.topic_subs SET err_cnt = COALESCE(rec.err_cnt, 0) + 1 WHERE tag = rec.tag AND ip = rec.ip;
+        END IF;
+
         INSERT INTO arc_energo.topic_msg_queue(ip, msg_id, status, sent_result)
         VALUES(rec.ip, NEW.msg_id, 
             CASE WHEN res='' THEN 10 ELSE 20 END,
