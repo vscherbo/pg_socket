7d7a62465c942be35beb452a9e86d977e46d7537
Vladimir Scherbo <vscherbo@gmail.com>
2019-11-26 15:43:21 +0300

    подтверждение до постановки в очередь

--- cr-topic_msg_received.sql
+++ cr-topic_msg_received.sql
@@ -2,7 +2,12 @@ CREATE OR REPLACE FUNCTION arc_energo.topic_msg_received(arg_msg_id integer)
  RETURNS void
  LANGUAGE sql
 AS $function$
+INSERT INTO arc_energo.topic_msg_queue(ip, msg_id, dt_delivered, status, sent_result)
+        VALUES(inet_client_addr(), arg_msg_id, clock_timestamp(), 90, 'before queued') 
+ON CONFLICT (ip, msg_id) DO UPDATE SET dt_delivered = clock_timestamp(), status=90;
+/**
 UPDATE topic_msg_queue SET dt_delivered = clock_timestamp(), status=90
 WHERE msg_id=arg_msg_id and ip = inet_client_addr() ;
+**/
 $function$
 ;
