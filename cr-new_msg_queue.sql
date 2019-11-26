d4f22bb82eb85746857730535ef9b2320177f194
Vladimir Scherbo <vscherbo@gmail.com>
2019-11-26 15:39:15 +0300

    второй способ подписки

---
+++ cr-new_msg_queue.sql
@@ -0,0 +1,26 @@
+CREATE OR REPLACE FUNCTION arc_energo.new_msg_queue(tag_in character varying, ip_in inet, port_in integer, msg_id_in integer)
+ RETURNS void
+ LANGUAGE plpgsql
+AS $function$
+
+DECLARE 
+res varchar;
+host_to text;
+BEGIN
+        host_to := host(ip_in::inet);
+        res := sock_send(host_to, port_in, msg_id_in::varchar);
+
+       INSERT INTO arc_energo.topic_msg_queue(ip, msg_id, status, sent_result)
+        VALUES(ip_in::inet, msg_id_in, 
+            CASE WHEN res='' THEN 10 ELSE 20 END,
+            res)
+        ON CONFLICT (ip, msg_id) DO NOTHING; -- received before registered
+
+        IF res = '' THEN
+            UPDATE arc_energo.topic_subs SET err_cnt = 0 WHERE tag::text = tag_in AND ip = ip_in;
+        ELSIF res LIKE '%timed out%' THEN
+            UPDATE arc_energo.topic_subs SET err_cnt = COALESCE(err_cnt, 0) + 1 WHERE tag::text = tag_in AND ip = ip_in;
+        END IF;
+END;
+$function$
+;
--- cr-trfn_new_topic_msg.sql
+++ cr-trfn_new_topic_msg.sql
@@ -4,30 +4,18 @@ CREATE OR REPLACE FUNCTION arc_energo.new_topic_msg()
 AS $function$
 DECLARE 
 loc_port integer;
-host_to text;
 rec RECORD;
-res varchar;
+
 BEGIN
+
 SELECT port INTO loc_port FROM arc_energo.topic WHERE tag = NEW.tag;
 
-    FOR rec IN SELECT tag, ip, err_cnt FROM arc_energo.topic_subs WHERE tag = NEW.tag AND (err_cnt < 3 OR err_cnt IS NULL)
+FOR rec IN SELECT tag, ip, err_cnt FROM arc_energo.topic_subs WHERE tag = NEW.tag AND (err_cnt < 3 OR err_cnt IS NULL)
     LOOP
-        host_to := host(rec.ip);
-        -- res := NULLIF(sock_send(host_to, loc_port, NEW.msg_id::varchar), '');
-        res := sock_send(host_to, loc_port, NEW.msg_id::varchar);
-        INSERT INTO arc_energo.topic_msg_queue(ip, msg_id, status, sent_result)
-        VALUES(rec.ip, NEW.msg_id, 
-            CASE WHEN res='' THEN 10 ELSE 20 END,
-            res);
-
-        IF res = '' THEN
-            UPDATE arc_energo.topic_subs SET err_cnt = 0 WHERE tag = rec.tag AND ip = rec.ip;
-        ELSIF res LIKE '%timed out%' THEN
-            UPDATE arc_energo.topic_subs SET err_cnt = COALESCE(rec.err_cnt, 0) + 1 WHERE tag = rec.tag AND ip = rec.ip;
-        END IF;
-
+		PERFORM new_msg_queue (rec.tag, rec.ip, loc_port, NEW.msg_id);
     END LOOP;
-RETURN NEW;
+
+   RETURN NEW;
 END;
 $function$
 ;
